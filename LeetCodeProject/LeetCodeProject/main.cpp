1759655542ea264c8e093bc90a96d4098fdba7fa
Nagrand <165293941@qq.com>
2019-04-11 22:23:26 +0800

    Xcode 运行是符合要求的，但是LeetCode一直报错

--- LeetCodeProject/LeetCodeProject/main.cpp
+++ LeetCodeProject/LeetCodeProject/main.cpp
@@ -7,9 +7,59 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
+//10. 正则表达式匹配
+bool isMatch(string s, string p) {
+    size_t pos = p.find('*');
+    if (pos != p.npos) {
+        //有*
+        int pIdx = 0;
+        for (int i = 0; i < s.size();) {
+            if (s[i] != p[pIdx] && p[pIdx] != '.') {
+                if (p[pIdx] == '*') {
+                    if (pIdx + 1 < p.size() && p[pIdx + 1] == s[i]) {
+                        pIdx++;
+                        i++;
+                        continue;
+                    }
+                    if (s[i] != p[pIdx - 1]) {
+                        return false;
+                    }
+                } else if (pIdx + 1 < p.size() && p[pIdx + 1] == '*') {
+                    if (pIdx + 2 < p.size()) {
+                        pIdx += 2;
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            if (p[pIdx] != '*') {
+                pIdx++;
+            }
+            i++;
+        }
+        return true;
+    } else if (s.size() != p.size()) {
+        return false;
+    } else {
+        //没有‘*’ 且长度相等
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != p[i] && p[i] != '.') {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    if (isMatch("babbcaacbccbbbbbabb", "bb*.b*b*a*aba*c")) {
+        printf("true\n");
+    } else {
+        printf("false\n");
+    }
     return 0;
 }
