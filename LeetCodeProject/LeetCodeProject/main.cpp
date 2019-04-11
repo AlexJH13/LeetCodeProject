6ce7df1773a90066a1c961162e1ed34a9a1b5515
Nagrand <165293941@qq.com>
2019-04-11 21:24:06 +0800

    git init

---
+++ LeetCodeProject/LeetCodeProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCodeProject
+//
+//  Created by 夏阳 on 2019/4/11.
+//  Copyright © 2019 夏阳. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
